65d94fcab8dfea3f254c85894b39f991d2f4852d
Atul Bansal <30015420+atulbansal4444@users.noreply.github.com>
2019-11-20 00:18:38 +0530

    Two Sum Q done.

---
+++ (1)TwoSum.java
@@ -0,0 +1,20 @@
+// 1. Two Sum
+
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int arr[]=new int[2];
+        for (int i=0; i<nums.length-1; i++) {
+            int diff = target-nums[i];
+            for(int j=i+1; j<nums.length; j++) {
+               if(diff==nums[j]) {
+                   arr[0]=i;
+                   arr[1]=j;
+                   return arr;
+               }
+            }
+        }
+        return arr;
+    }
+}
