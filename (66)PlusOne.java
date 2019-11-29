5369addcf40c53f2f22e075c66c8d336ac12499f
Atul Bansal <30015420+atulbansal4444@users.noreply.github.com>
2019-11-30 00:03:09 +0530

    Plus One Added with less Space Complexity

---
+++ (66)PlusOne.java
@@ -0,0 +1,26 @@
+//66. Plus One
+
+class Solution {
+    public int[] plusOne(int[] digits) {
+        int carry = 1;
+        for(int i=digits.length-1;i>=0;i--)
+        {
+            if(digits[i]+carry == 10)
+            {
+                digits[i]=0;
+                carry = 1;
+            }
+            else
+            {
+                digits[i]=digits[i]+carry;
+                carry = 0;
+            }
+        }
+        if(carry == 1)
+        {
+            digits = Arrays.copyOf(digits, digits.length + 1);
+            digits[0] = carry;
+        }
+        return digits;
+    }
+}
