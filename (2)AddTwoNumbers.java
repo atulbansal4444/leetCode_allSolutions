370ee34492c6ba2c0c10e6328058a6700be89e3c
Atul Bansal <30015420+atulbansal4444@users.noreply.github.com>
2019-12-03 23:38:58 +0530

    Update (2)AddTwoNumbers.java

--- (2)AddTwoNumbers.java
+++ (2)AddTwoNumbers.java
@@ -16,29 +16,27 @@ class Solution
         ListNode dummyNode = new ListNode(0);
         ListNode p = l1, q = l2, curr = dummyNode;
         int carry = 0;
-        while(p!=null || q!=null)
+        while(p != null || q != null)
         {
             int x = (p!=null) ? p.val : 0;
             int y = (q!=null) ? q.val : 0;
             int sum = x + y + carry;
             carry = sum/10;
             curr.next = new ListNode(sum%10);
-            if(p!=null)
+            if(p != null)
             {
                 p=p.next;
             }
-            if(q!=null)
+            if(q != null)
             {
-                q=q.next;
+                q = q.next;
             }
-            curr=curr.next;
-            
+            curr = curr.next;
         }
         if(carry>0)
         {
             curr.next = new ListNode(carry);
         }
         return dummyNode.next;
-        
     }
 }
