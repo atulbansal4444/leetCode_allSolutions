46939db1b528e2f1a90efe9f53287eaf8245dee4
Atul Bansal <30015420+atulbansal4444@users.noreply.github.com>
2019-12-05 13:26:37 +0530

    Single Number q added

---
+++ (136)SingleNumber.java
@@ -0,0 +1,12 @@
+//136. Single Number
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int x = 0;
+      for(int i=0; i<nums.length;i++)
+      {
+        x = x^nums[i];
+      }
+      return x;
+    }
+}
